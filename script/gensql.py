b5c694e935dc0c9dcf927699e8955711ee37bdbe
LyleMi <1278919492@qq.com>
2016-04-13 15:23:38 +0800

    update

--- script/gensql.py
+++ script/gensql.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 '''
 Mysql Version: 14.14 Distrib 5.6.28, for debian-linux-gnu (x86_64) using  EditLine wrapper
 
@@ -7,36 +7,25 @@ Mysql Version: 14.14 Distrib 5.6.28, for debian-linux-gnu (x86_64) using  EditLi
 import random
 import os
 
-UNIQUE_QUERY_NUM = 1000
 TEST_DATABASE = 'test'
-TEST_TABLE = 'test3'
-READ_COLOMN = 'a'
-INSERT_COLUMN = 'b'
+TEST_TABLE = 'test1'
+INSERT_COLUMN = 'id'
 UPDATE_COLUMN = 'c'
-QUERY_FILE = '/tmp/answersql'
-INSERT_RECORD_FILE = '/tmp/insertquery'
-OUTFILE = 'out.sql'
-TOTAL_NUM = 1000000
+INSERT_RECORD_FILE = '/tmp/4insertquery'
+OUTFILE = '43.sql'
+TOTAL_NUM = 10000
 
-def genread(totalNum, uniqueNum = None):
-    if uniqueNum == None:
-        uniqueNum = totalNum
-    if uniqueNum > UNIQUE_QUERY_NUM:
-        raise Exception("Exceed the number of all records.")
-    with open(QUERY_FILE, 'r') as f:
+def genread(totalNum):
+    with open(INSERT_RECORD_FILE, 'r') as f:
         answerSeq = f.read().split()
-    randomAnswerSeq = random.sample(answerSeq, uniqueNum)
     for _ in xrange(totalNum):
-        toquery = random.choice(randomAnswerSeq)
-        yield 'select * from {}.{} where {}="{}";\n'.format(TEST_DATABASE, TEST_TABLE, READ_COLOMN, toquery)
+        toquery = random.choice(answerSeq)
+        yield 'select * from {}.{} where {}="{}";\n'.format(TEST_DATABASE, TEST_TABLE, INSERT_COLUMN, toquery)
 
 def genwrite(totalNum):
-    f = open(INSERT_RECORD_FILE, 'a')
     for _ in xrange(totalNum):
         toinsert = os.urandom(50).encode('hex')
-        f.write(toinsert+'\n')
         yield 'insert into {}.{}({}) values ("{}");\n'.format(TEST_DATABASE, TEST_TABLE, INSERT_COLUMN, toinsert)
-    f.close()
 
 def gendelete(totalNum):
     with open(INSERT_RECORD_FILE, 'r') as f:
@@ -58,10 +47,12 @@ def genupdate(totalNum):
         yield 'update {}.{} set {}="{}" where {}="{}";\n'.format(TEST_DATABASE, TEST_TABLE, UPDATE_COLUMN, toupdate, INSERT_COLUMN, toquery)
 
 def gencreate(idNum, eachNum=1):
+    f = open(INSERT_RECORD_FILE, 'w')
     tmp = []
-    num = 10000 / eachNum
+    num = 1 / eachNum
     for i in xrange(idNum):
         ID = 'user' + os.urandom(9).encode('hex')
+        f.write(ID+'\n')
         for __ in xrange(eachNum):
             tmp += ['("{}", "{}", "{}", "{}", "{}")'.\
                      format(ID, os.urandom(50).encode('hex'), os.urandom(50).encode('hex'), os.urandom(50).encode('hex'), os.urandom(50).encode('hex'))]
@@ -69,16 +60,19 @@ def gencreate(idNum, eachNum=1):
             yield 'insert into {}.{} values {};\n'.format(TEST_DATABASE, TEST_TABLE, ','.join(tmp))
             tmp = []
 
-#CONFIG = {genread: 0.9, genwrite: 0.1}
-#CONFIG = {genwrite: 1}
-#CONFIG = {genupdate: 0.1, gendelete: 0.1}
-CONFIG = {gencreate: 1}
+#CONFIG = {gencreate: 1}
+#CONFIG = {genread: 0.1, genupdate: 0.9}
+CONFIG = {genread: 0.6,genupdate: 0.1,genwrite: 0.3}
+#CONFIG = {genread: 0.9,genupdate: 0.1}
+#CONFIG = {genread: 1}
+
 
 if __name__ == '__main__':
     randbox = []
     for i in CONFIG:
         randbox += [i(int(CONFIG[i] * TOTAL_NUM))] * int(CONFIG[i] * TOTAL_NUM + 1)
     random.shuffle(randbox)
+    #print randbox
     fout = open(OUTFILE, 'w+')
     for gen in randbox:
         try:
