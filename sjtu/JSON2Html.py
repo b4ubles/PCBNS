f0598f97a765e16fc5017511632d719032ee8d9a
ZHANG ZHUO <zhangzhuo@sjtu.edu.cn>
2016-04-15 12:55:56 +0800

    Fixbug of JSON2Html.py

--- sjtu/JSON2Html.py
+++ sjtu/JSON2Html.py
@@ -19,27 +19,29 @@ def JSON2Html( key, zz_type ):
     if isinstance(zz_type, dict):
         tag_name = key
         tag_page = ''
+        if zz_type.has_key( 'script' ):
+            tag_page = tag_page + JSON2Html( 'script', zz_type['script'] )
         for sub_key in zz_type:
-            if (sub_key[0] == '@'):
-                zz_content = ''
-                if (sub_key == '@content' ):
-                    zz_content = 'text/html; charset=utf-8'
+            if (sub_key != 'script'):
+                if (sub_key[0] == '@'):
+                    zz_content = ''
+                    if (sub_key == '@content' ):
+                        zz_content = 'text/html; charset=utf-8'
+                    else:
+                        zz_content = zz_decode(zz_type[sub_key])
+                    zz_content = zz_content.replace("['","")
+                    zz_content = zz_content.replace("']","")
+                    zz_content = zz_content.replace("', '"," ")
+                    if ((sub_key == '@src' or sub_key == '@href') and (zz_content != "")):
+                        if ( zz_content[0] == '/' ):
+                            zz_content = url_1 + zz_content
+                        elif ( zz_content[:3] != 'http' ):
+                            zz_content = url_2 + zz_content
+                    tag_name = tag_name + ' '
+                    tag_name = tag_name + sub_key[1:] + '="'
+                    tag_name = tag_name + zz_content + '"'
                 else:
-                    zz_content = zz_decode(zz_type[sub_key])
-                zz_content = zz_content.replace("'","")
-                zz_content = zz_content.replace("[","")
-                zz_content = zz_content.replace("]","")
-                zz_content = zz_content.replace(","," ")
-                if ((sub_key == '@src' or sub_key == '@href') and (zz_content != "")):
-                    if ( zz_content[0] == '/' ):
-                        zz_content = url_1 + zz_content
-                    elif ( zz_content[:3] != 'http' ):
-                        zz_content = url_2 + zz_content
-                tag_name = tag_name + ' '
-                tag_name = tag_name + sub_key[1:] + '="'
-                tag_name = tag_name + zz_content + '"'
-            else:
-                tag_page = tag_page + JSON2Html( sub_key, zz_type[sub_key] )
+                    tag_page = tag_page + JSON2Html( sub_key, zz_type[sub_key] )
         outp = ' <' + tag_name + '> ' + tag_page + ' </' + key + '> '
         return outp
     if isinstance(zz_type, list):
